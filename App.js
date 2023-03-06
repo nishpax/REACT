703443e150e3fcd2e44e4dea6d4eab26d066fa24
nishpax <126654185+nishpax@users.noreply.github.com>
2023-03-06 22:16:05 +0530

    my first react

---
+++ App.js
@@ -0,0 +1,7 @@
+//import logo from './logo.svg';
+//import './App.css';
+import { One,Two,Three,Four } from "./f1";
+function App() {
+  }
+
+export default App;
---
+++ f1.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+export function One(){
+    return(
+ <h1>
+    I am heading one ... from first component
+ </h1>
+)
+}
+ 
+export function Two(){
+  return(
+  <h2>
+ i am heading two ....from second component
+   </h2>
+  )
+}
+
+export function Three(){
+   return(
+   <h3>
+  i am heading Three ....from Third component
+    </h3>
+   )
+ }
+
+ export function Four(){
+   return(
+   <button> click me</button>
+
+   )
+ }
